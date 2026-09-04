2d7adeffda6dc560d5b1521c1d542bc200a69238
Guillaume Amselle Andreux <guillaume@andreux.fr>
2026-09-04 16:56:48 +0200

    fix sentry code

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import * as Sentry from "@sentry/react";
-import { browserTracingIntegration, replayIntegration } from "@sentry/browser";
 
 // On configure Sentry UNIQUEMENT en production
 // On utilise la variable `import.meta.env.PROD`, fournie automatiquement par Vite
@@ -14,20 +13,20 @@ if (import.meta.env.PROD) {
         integrations: [
             // Cette intégration active le monitoring de performance.
             // Elle permet de voir, par exemple, combien de temps prennent les appels API.
-            browserTracingIntegration(),
+            Sentry.browserTracing(),
 
             // Cette intégration enregistre les sessions utilisateur où une erreur se produit.
             // C'est comme avoir une "vidéo" de ce que l'utilisateur a fait avant le bug.
-            replayIntegration()
+            Sentry.replay()
         ],
 
         // On configure le taux d'échantillonnage pour la performance.
         // 1.0 signifie qu'on capture 100% des transactions pour analyse.
-        tracesSampleRate: 1.0,
+        traces_sample_rate: 1.0,
 
         // On configure le taux d'échantillonnage pour les "replays" de session.
-        replaysSessionSampleRate: 0.1,  // On enregistre 10% des sessions.
-        replaysOnErrorSampleRate: 1.0,  // Mais on enregistre 100% des sessions où une erreur survient.
+        replays_session_sample_rate: 0.1,  // On enregistre 10% des sessions.
+        replays_on_error_sample_rate: 1.0,  // Mais on enregistre 100% des sessions où une erreur survient.
     });
 }
 
